a4ca5a597d44e2d11a0306a79e344133d7fe0774
Harlan Haskins <harlan@harlanhaskins.com>
2017-04-19 15:52:12 -0400

    Add Xcode project for drop-in use

---
+++ Xcode/Clang/Clang.h
@@ -0,0 +1,19 @@
+//
+//  Clang.h
+//  Clang
+//
+//  Created by Harlan Haskins on 4/19/17.
+//  Copyright © 2017 Trill Lang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Clang.
+FOUNDATION_EXPORT double ClangVersionNumber;
+
+//! Project version string for Clang.
+FOUNDATION_EXPORT const unsigned char ClangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clang/PublicHeader.h>
+
+
