82c0c75070b195f576d123aa9bcc4c1110ca7489
Harlan Haskins <harlan@harlanhaskins.com>
2017-04-23 12:19:14 -0400

    Make File and UniqueFileID hashable.

--- Sources/Clang/File.swift
+++ Sources/Clang/File.swift
@@ -4,16 +4,22 @@ import cclang
 import Foundation
 
 /// Represents a file ID that's unique to each file in a translation unit.
-public struct UniqueFileID: Equatable {
+public struct UniqueFileID: Equatable, Hashable {
     let clang: CXFileUniqueID
 
     public static func ==(lhs: UniqueFileID, rhs: UniqueFileID) -> Bool {
         return lhs.clang.data == rhs.clang.data
     }
+
+    /// A unique integer value representing this unique ID.
+    public var hashValue: Int {
+        return Int(Int64(bitPattern: clang.data.0 ^ clang.data.1 ^ clang.data.2)
+             ^ 0x0a28bf1ac) // xor with a constant for extra mixing.
+    }
 }
 
 /// A particular source file that is part of a translation unit.
-public struct File: Equatable {
+public struct File: Equatable, Hashable {
     let clang: CXFile
 
     /// Retrieve the complete file and path name of the given file.
@@ -39,5 +45,14 @@ public struct File: Equatable {
     /// Determines if two files are equal.
     public static func ==(lhs: File, rhs: File) -> Bool {
         return clang_File_isEqual(lhs.clang, rhs.clang) != 0
+    }
+
+    /// A unique integer value representing this file.
+    public var hashValue: Int {
+        let mixin = 0xa24b1bc4
+        guard let uniqueID = uniqueID else {
+            return name.hashValue ^ mixin
+        }
+        return uniqueID.hashValue ^ mixin
     }
 }
